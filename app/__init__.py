2af09f11ecdebae5f833f7f2317c065333bda66e
Wensheng <wensheng9131@gmail.com>
2024-11-10 22:17:06 +0800

    優化: 新增靜態檔案快取設定

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import os
+from datetime import timedelta
 
 db = SQLAlchemy()
 
@@ -9,15 +10,24 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.urandom(24)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workhours.db'
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000
 
     db.init_app(app)
+
+    @app.route('/static/<path:filename>')
+    def static_files(filename):
+        response = send_from_directory(app.static_folder, filename)
+        response.cache_control.max_age = 31536000
+        response.cache_control.public = True
+        response.headers['Vary'] = 'Accept-Encoding'
+        return response
 
     from .routes import register_routes
     register_routes(app)
 
     with app.app_context():
         db.create_all()
-        # 檢查是否需要建立預設設定
         from .models import Settings
         if not Settings.query.first():
             default_settings = Settings()
